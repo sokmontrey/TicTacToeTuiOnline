d577404aa7a2d062f8fc8b6b70c96c06c1754578
Sokmontrey <sythatsokmontrey@gmail.com>
2025-01-18 16:28:24 -0500

    feat: CreateRoom
    TODO: implement Room.Run()

--- internal/server/network/lobby.go
+++ internal/server/network/lobby.go
@@ -2,15 +2,53 @@ package network
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/sokmontrey/TicTacToeTuiOnline/internal/server/utils"
+	"github.com/sokmontrey/TicTacToeTuiOnline/pkg"
+	"strconv"
 	"sync"
 )
 
+const (
+	maxPlayers  = 3
+	numIdDigits = 4
+)
+
 type Lobby struct {
-	rooms map[RoomId]*Room
+	rooms map[string]*Room
 	mu    sync.Mutex
 }
 
+func NewLobby() *Lobby {
+	return &Lobby{
+		rooms: make(map[string]*Room),
+	}
+}
+
+func (l *Lobby) GenerateId(digits int) string {
+	id := pkg.GenerateId(digits)
+	_, ok := l.rooms[id]
+	for ok {
+		id = pkg.GenerateId(digits)
+		_, ok = l.rooms[id]
+	}
+	return id
+}
+
 func (l *Lobby) CreateRoom(c *gin.Context) {
+	responder := utils.NewHttpResponder(c)
+	numPlayersParam := c.Param("numPlayers")
+	numPlayers, err := strconv.Atoi(numPlayersParam)
+	if err != nil {
+		responder.NumPlayersParamError(numPlayersParam)
+	} else if numPlayers > maxPlayers {
+		responder.MaxNumPlayerError(maxPlayers)
+	}
+	l.mu.Lock()
+	id := l.GenerateId(numIdDigits)
+	l.rooms[id] = NewRoom(numPlayers, id)
+	go l.rooms[id].Run()
+	responder.RoomCreated(id)
+	l.mu.Unlock()
 }
 
 func (l *Lobby) JoinRoom(c *gin.Context) {
