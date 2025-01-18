49463c2a7289a4a19c805e87cef852a8abea847b
Sokmontrey <sythatsokmontrey@gmail.com>
2025-01-17 21:47:46 -0500

    project file structure

---
+++ cmd/server/main.go
@@ -0,0 +1,5 @@
+﻿package main
+
+func main() {
+
+}
---
+++ pkg/vec2/vec2.go
@@ -0,0 +1,38 @@
+﻿package vec2
+
+type Vec2 struct {
+	X int
+	Y int
+}
+
+func New(x int, y int) Vec2 {
+	return Vec2{x, y}
+}
+
+func Zero() Vec2 {
+	return Vec2{0, 0}
+}
+
+func (v *Vec2) Add(other Vec2) Vec2 {
+	return Vec2{v.X + other.X, v.Y + other.Y}
+}
+
+func (v *Vec2) Sub(other Vec2) Vec2 {
+	return Vec2{v.X - other.X, v.Y - other.Y}
+}
+
+func (v *Vec2) Up() Vec2 {
+	return Vec2{v.X, v.Y - 1}
+}
+
+func (v *Vec2) Down() Vec2 {
+	return Vec2{v.X, v.Y + 1}
+}
+
+func (v *Vec2) Left() Vec2 {
+	return Vec2{v.X - 1, v.Y}
+}
+
+func (v *Vec2) Right() Vec2 {
+	return Vec2{v.X + 1, v.Y}
+}
